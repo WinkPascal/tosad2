901a2b6cb50f9aeef878677eaf2e7693fa29101e
ankaandonova <anka.andonova@student.hu.nl>
2020-01-28 13:41:16 +0100

    :)

--- src/main/java/domain/businessRuleGenerator/AttributeRule/AttributeListRule.java
+++ src/main/java/domain/businessRuleGenerator/AttributeRule/AttributeListRule.java
@@ -31,7 +31,7 @@ public class AttributeListRule implements BusinessRuleStrategy {
 			+   "	attribute " +entiteit+"."+attribute+"%type := :NEW."+attribute+"; \n"
 			+   "BEGIN \n"
 			+   "	IF attribute NOT IN "+getList()+ " THEN \n"
-			+   "		Raise_Application_Error(-20343," +  attribute + " isn't found in " + getList() + " ); \n"
+			+   "		Raise_Application_Error(-20343,'" +  attribute + " is not found in " + getList() + "' ); \n"
 			+   "		ROLLBACK; \n"
 			+   "	END IF; \n"
 			+   "END "+ruleId;
