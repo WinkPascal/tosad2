c615015422950b7bb9d1d8a4108ec4d55d5de6a9
ankaandonova <anka.andonova@student.hu.nl>
2020-02-02 13:04:00 +0100

    :8ball:

--- src/main/java/domain/businessRuleGenerator/AttributeRule/AttributeOtherRule.java
+++ src/main/java/domain/businessRuleGenerator/AttributeRule/AttributeOtherRule.java
@@ -3,10 +3,32 @@ package domain.businessRuleGenerator.AttributeRule;
 import domain.businessRuleGenerator.BusinessRuleStrategy;
 
 public class AttributeOtherRule implements BusinessRuleStrategy {
-	
+
+	private String ruleId;
+	private String invoerveld;
+	private String entiteit;
+
+	public AttributeOtherRule(String ruleId, String invoerveld, String entiteit) {
+		this.ruleId = ruleId;
+		this.invoerveld = invoerveld;
+		this.entiteit = entiteit;
+	}
+
+
 	@Override
 	public String createBusinessRule() {
-		return null;
+		String trigger=
+				"CREATE OR REPLACE TRIGGER "+ruleId+ " \n"+
+				"	BEFORE insert or update \n"+
+				"	ON "+entiteit+
+				"   FOR EACH ROW \n "+
+				"BEGIN \n" +
+				"	IF "+ invoerveld + "THEN \n" +
+				"		Raise_Application_Error (-20343, '"+ invoerveld+ " '); \n" +
+				"		ROLLBACK; \n"+
+				"	END IF; \n" +
+				"END;";
+		return trigger;
 	}
 
 }
